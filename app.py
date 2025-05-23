fa4dcbd2e29de63acdded220aee8ef8bb2373f2d
EmbeddEdge <turynlb@gmail.com>
2025-05-23 09:41:06 +0200

    updates

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    mountains = ['Everest', 'K2', 'Kilimanjaro', 'Lhotse', 'Makalu']
     return render_template('index.html', mountain = 'Everest')
 
 #if __name__ == "__main__":
